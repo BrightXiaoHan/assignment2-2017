4dcd62500493cbed97263915dbb3a62f59e7e92f
hanbing <beatmight@gmail.com>
2020-03-21 07:27:36 +0800

    finish DLGpuRelu

--- src/gpu_op.cu
+++ src/gpu_op.cu
@@ -323,8 +323,35 @@ int DLGpuMatrixMultiply(const DLArrayHandle matA, bool transposeA,
   return 0;
 }
 
+__global__ void relu_kernel(array_size_t size, const float *input, float *output){
+  array_size_t index = blockIdx.x * blockDim.x + threadIdx.x;
+  float input_val = input[index];
+  if (input[index] > 0)
+    output[index] = input_val;
+  else
+    output[index] = 0;
+}
+
 int DLGpuRelu(const DLArrayHandle input, DLArrayHandle output) {
   /* TODO: Your code here */
+  assert(input->ndim == output->ndim);
+  for (int i = 0; i < output->ndim; i++){
+    assert(output->shape[i] == input->shape[i]);
+  }
+  
+  array_size_t size = output->size();
+
+  dim3 threads;
+  dim3 blocks;
+  if (size <= 1024){
+    threads.x = size;
+    blocks.x = 1;
+  }else{
+    threads.x = 1024;
+    blocks.x = (size + 1023) / 1024;
+  }
+
+  relu_kernel<<<blocks, threads>>>(size, (const float *)input->data, (float *)output->data);
   return 0;
 }
 
